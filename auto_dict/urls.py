dc6c68570392febefb80f31f06340eabd1a21e9d
Kalu Kalu <tmipanthers@gmail.com>
2017-03-16 16:00:24 +0800

    added functionality for dict search

--- auto_dict/urls.py
+++ auto_dict/urls.py
@@ -1,6 +1,9 @@
 
 from django.conf.urls import url
 
+
+app_name = 'auto_dict'
+
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
--- auto_dict/views.py
+++ auto_dict/views.py
@@ -1,5 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+try:
+    from urllib2 import urlopen
+except:
+    from urllib.request import urlopen
+
+
+def make_url(word):
+    url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/"
+    url += word + "?key=05b928bf-c814-4ee3-9762-020bf3dc8c2b"
+    return url
+
+
 def index(request):
-    return HttpResponse("Hello David. The app is on it's way.")
+
+    if request.method == 'POST':
+
+        word = request.POST.get('word', '')
+        if word:
+            word = word.strip()
+
+        url = make_url(word)
+
+        # import pdb
+        # pdb.set_trace()
+
+        html = urlopen(url)
+        text = html.read()
+
+        index = text.find("def")
+        index = text.find("dt", index)
+        index = text.find(":", index)
+        end_index = text.find("</dt>", index)
+
+        my_def = text[index:end_index]
+
+        context = {'word': word, 'definition' : my_def}
+
+        
+
+        return render(request, 'auto_dict/index.html', context)
+
+        
+
+
+    return render(request, 'auto_dict/index.html')
